01b48fe3c55dc3227c5fa8144c61d603ca41cf01
David_Stichter <david.stichter3414@gmail.com>
2015-11-23 16:11:02 -0500

    Fixed Formating, Added Alert, CheckBox removes properly

--- js/create.js
+++ js/create.js
@@ -1,18 +1,23 @@
 $(document).ready(function(){
-
-$(".btn-primary").on('click', function(e){
+$("#alertField").hide()
+$("#addTodo").on("focus", function(){
+$("#alertField").hide()
+});
+$(".btn-block").on('click', function(e){
   e.preventDefault();
-
+  if(formCheck() === false){
+    return;
+  }
   var toAdd = $("#addTodo").val();
 
   var newRow = $("<tr>");
   var createCheckBox = $("<input type='checkbox'>").addClass("checkbox");
   var createDeleteBox = $("<button>").addClass("btn btn-danger").append("X")
-  var todoData = $("<td>").append(toAdd)
-  var todoCB = $("<td>").append(createCheckBox);
-  var todoDB = $("<td>").append(createDeleteBox);
+  var todoData = $("<td>").append(toAdd).addClass("col-md-10")
+  var todoCB = $("<td>").append(createCheckBox).addClass("col-md-1");
+  var todoDB = $("<td>").append(createDeleteBox).addClass("col-md-1");
   newRow.append(todoData).append(todoCB).append(todoDB);
-  $("tbody").append(newRow);
+  $("#todoTable").append(newRow);
 });
 $("table").on("click", ".btn-danger", function() {
   $(this).parent().parent().remove();
@@ -23,12 +28,22 @@ $("table").on("click", ".checkbox", function() {
   var td = row.find("td:first")
   if(this.checked === true){
     td.addClass("strike");
+    $(this).parent().remove();
     return
   }
-  if(this.checked === false){
-    td.removeClass("strike");
-  }
+
+
 
 });
 
+function formCheck() {
+  if($("#addTodo").val() === ""){
+    $("#alertField").show()
+    return false
+  }
+  else{
+    return true
+  }
+  
+}
 });
